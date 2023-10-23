92287bfabb3a5dc8ee2090a5dac3655395c39deb
Thuận Chung Lương <thuanchung85@gmail.com>
2023-10-23 11:59:31 +0700

    save

--- Sources/GoiAPI3/LockScreenHaveConfirmPIN.swift
+++ Sources/GoiAPI3/LockScreenHaveConfirmPIN.swift
@@ -21,7 +21,7 @@ public struct LockScreenHaveConfirmPIN : View {
         if (isShowConFirmPassCodeView == false){
             VStack{
                 
-                Image(systemName: "multiply.circle.fill")
+                Image(systemName: "person")
                     .resizable()
                     .frame(width: 50, height: 50)
                     .padding(.top,20)
@@ -68,7 +68,7 @@ public struct LockScreenHaveConfirmPIN : View {
         else{
             VStack{
                 
-                Image(systemName: "multiply.circle.fill")
+                Image(systemName: "person.fill")
                     .resizable()
                     .frame(width: 50, height: 50)
                     .padding(.top,20)
