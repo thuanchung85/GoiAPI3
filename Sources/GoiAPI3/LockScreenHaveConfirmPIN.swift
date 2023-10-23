7cf35123795bf6ec767a8afb84e455600823dffe
Thuận Chung Lương <thuanchung85@gmail.com>
2023-10-23 11:31:50 +0700

    save

--- Sources/GoiAPI3/LockScreenHaveConfirmPIN.swift
+++ Sources/GoiAPI3/LockScreenHaveConfirmPIN.swift
@@ -5,7 +5,8 @@ import SwiftUI
 public struct LockScreenHaveConfirmPIN : View {
     
     @Binding var password:String
- 
+    @State var isShowConFirmPassCodeView:Bool = false
+    
     var textAskUserDo:String
     
     public init(textAskUserDo:String,password: Binding<String>) {
@@ -14,50 +15,100 @@ public struct LockScreenHaveConfirmPIN : View {
     }
     
     public var body: some View{
-        
-        VStack{
-            
-            Image(systemName: "global.fill")
-                .resizable()
-                .frame(width: 50, height: 50)
-                .padding(.top,20)
-            
-            Text(textAskUserDo)
-                .font(.title2)
-                .fontWeight(.heavy)
-                .padding(.top,20)
-            
-            HStack(spacing: 22){
+        //Bước 1: hiện page cho user nhập mã PIN trước
+        if (isShowConFirmPassCodeView == false){
+            VStack{
                 
-                // Password Circle View...
+                Image(systemName: "star.fill")
+                    .resizable()
+                    .frame(width: 50, height: 50)
+                    .padding(.top,20)
                 
-                ForEach(0..<6,id: \.self){index in
+                Text(textAskUserDo)
+                    .font(.title2)
+                    .fontWeight(.heavy)
+                    .padding(.top,20)
+                
+                HStack(spacing: 22){
                     
-                    PasswordView(index: index, password: $password)
-                }
-            }
-            // for smaller size iphones...
-            .padding(.top,10)
-            
-            
-            LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 3),spacing: UIScreen.main.bounds.width < 750 ? 5 : 15){
-                
-                // Password Button ....
-                
-                ForEach(1...9,id: \.self){value in
+                    // Password Circle View...
                     
-                    PasswordButton2(value: "\(value)",password: $password)
+                    ForEach(0..<6,id: \.self){index in
+                        
+                        PasswordView(index: index, password: $password)
+                    }
                 }
+                // for smaller size iphones...
+                .padding(.top,10)
                 
-                PasswordButton2(value: "delete.fill",password: $password)
                 
-                PasswordButton2(value: "0", password: $password)
+                LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 3),spacing: UIScreen.main.bounds.width < 750 ? 5 : 15){
+                    
+                    // Password Button ....
+                    
+                    ForEach(1...9,id: \.self){value in
+                        
+                        PasswordButton2(value: "\(value)",password: $password, isShowConFirmPassCodeView: $isShowConFirmPassCodeView)
+                    }
+                    
+                    PasswordButton2(value: "delete.fill",password: $password, isShowConFirmPassCodeView: $isShowConFirmPassCodeView)
+                    
+                    PasswordButton2(value: "0", password: $password, isShowConFirmPassCodeView: $isShowConFirmPassCodeView)
+                }
+                .padding(.bottom)
+                
             }
-            .padding(.bottom)
-
+            .navigationTitle("")
+            .navigationBarHidden(true)
         }
-        .navigationTitle("")
-        .navigationBarHidden(true)
+        
+        //Bước 2: hiện page cho user confirm mã PIN sau khi bước 1 ok
+        else{
+            VStack{
+                
+                Image(systemName: "star.fill")
+                    .resizable()
+                    .frame(width: 50, height: 50)
+                    .padding(.top,20)
+                
+                Text("Please Confirm your PIN number")
+                    .font(.title2)
+                    .fontWeight(.heavy)
+                    .padding(.top,20)
+                    .foregroundColor(.red)
+                
+                HStack(spacing: 22){
+                    
+                    // Password Circle View...
+                    
+                    ForEach(0..<6,id: \.self){index in
+                        
+                        PasswordView(index: index, password: $password)
+                    }
+                }
+                // for smaller size iphones...
+                .padding(.top,10)
+                
+                
+                LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 3),spacing: UIScreen.main.bounds.width < 750 ? 5 : 15){
+                    
+                    // Password Button ....
+                    
+                    ForEach(1...9,id: \.self){value in
+                        
+                        PasswordButton3(value: "\(value)",password: $password)
+                    }
+                    
+                    PasswordButton3(value: "delete.fill",password: $password)
+                    
+                    PasswordButton3(value: "0", password: $password)
+                }
+                .padding(.bottom)
+                
+            }
+            .navigationTitle("")
+            .navigationBarHidden(true)
+        }
     }
 }
 
@@ -66,7 +117,7 @@ struct PasswordButton2 : View {
     
     var value : String
     @Binding var password : String
-    
+    @Binding var isShowConFirmPassCodeView:Bool
     
     var body: some View{
         
@@ -115,13 +166,85 @@ struct PasswordButton2 : View {
                     
                     // Delay Animation...
                     
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
                         
                         withAnimation{
                             
                             if password.count == 6{
                                 
                                 print(password)
+                               //gọi tiếp view confirm passcode
+                                self.isShowConFirmPassCodeView = true
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct PasswordButton3 : View {
+    
+    var value : String
+    @Binding var password : String
+  
+    
+    var body: some View{
+        
+        Button(action: setPassword, label: {
+            
+            VStack{
+                
+                if value.count > 1{
+                    
+                    // Image...
+                    
+                    Image(systemName: "delete.left")
+                        .font(.system(size: 24))
+                        .foregroundColor(.blue)
+                }
+                else{
+                    
+                    Text(value)
+                        .font(.title)
+                        .foregroundColor(.red)
+                }
+            }
+            .padding()
+
+        })
+    }
+    
+    func setPassword(){
+        
+        // checking if backspace pressed...
+        
+        withAnimation{
+            
+            if value.count > 1{
+                
+                if password.count != 0{
+                    
+                    password.removeLast()
+                }
+            }
+            else{
+                
+                if password.count != 6{
+                    
+                    password.append(value)
+                    
+                    // Delay Animation...
+                    
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                        
+                        withAnimation{
+                            
+                            if password.count == 6{
+                                
+                                print("password confirm: ",password)
+                               //gọi tiếp view confirm passcode
                                
                             }
                         }
